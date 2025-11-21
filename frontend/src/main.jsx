5fedb701f507153c6dec07865f4f669d7ae9b414
amandarbaker24 <amandarbaker24@gmail.com>
2025-11-20 20:56:44 -0500

    frontend styling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,12 @@ import './index.css';
 
 // Import your page components
 import KitchenDashboard from './pages/KitchenDashboard.jsx';
+import Pantry from './pages/Pantry.jsx';
+import Recipes from './pages/Recipes.jsx';
+import MealPlan from './pages/MealPlan.jsx';
+import ShoppingList from './pages/ShoppingList.jsx';
+import AllergiesPreferences from './pages/AllergiesPreferences.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -16,6 +22,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />}>
           {/* When the path is exactly "/", render the KitchenDashboard */}
           <Route index element={<KitchenDashboard />} />
+          <Route path="pantry" element={<Pantry />} />
+          <Route path="recipes" element={<Recipes />} />
+          <Route path="planner" element={<MealPlan />} />
+          <Route path="shopping" element={<ShoppingList />} />
+          <Route path="allergies" element={<AllergiesPreferences />} />
+
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/AllergiesPreferences.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AllergiesPreferences() {
+  return <h1>Allergies & Food Preferences</h1>;
+}
+
+export default AllergiesPreferences;
